2b02c660ee27d565d92387dc493acca72aa3c7c3
beka88 <bekakadyrova@gmail.com>
2022-09-11 17:07:23 -0500

    "ab"

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -2,26 +2,10 @@ package tests;
 
 public class GoogleTests {
     public static void main(String[] args) {
-        
+
         //this is a comment
         System.out.println("I am here");
-        System.out.println("I am where");
-        System.out.println("Wow");
-        System.out.println("Meow");
-        System.out.println("Guff");
-        System.out.println(" Changes from remote");
-        System.out.println("Another changes from remote");
-        System.out.println("Changes from local");
-        System.out.println("Another Changes from local");
-        System.out.println("Another Changes from remote");
-        System.out.println("This is from Local");
 
-
-
-
-
-
-        System.out.println("Hello");
 
     }
 }
